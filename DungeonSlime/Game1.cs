e90e877174da7d2ebe80ec4f7d1d8a3cdd531f63
educrod <edu.crod@gmail.com>
2026-08-25 22:35:05 -0300

    adding transition effect shader code

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -34,6 +34,9 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
+        // Allow the Core class to also load content.
+        base.LoadContent(); 
+        
         // Load the background theme music.
         _themeSong = Content.Load<Song>("audio/theme");
     }
